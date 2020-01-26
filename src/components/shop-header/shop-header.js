e6ff71f57b747cb7f593773c485219f06c34546b
vlademorr <47037960+vlademorr@users.noreply.github.com>
2020-01-26 19:26:23 +0100

    Update shop-header.js

--- src/components/shop-header/shop-header.js
+++ src/components/shop-header/shop-header.js
@@ -6,7 +6,7 @@ const ShopHeader = ({ numItems, total }) => {
     return (
         <header className="shop-header row">
             <Link to="/">
-                <div className="logo text-dark">ReStore</div>
+                <div className="logo text-dark">BookStore</div>
             </Link>
             <Link to="/cart">
                 <div className="shopping-cart">
